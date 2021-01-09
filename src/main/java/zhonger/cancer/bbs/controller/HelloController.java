327adce8e51776212abcd34ea9a815c4395c8296
komaeka <komaeka@163.com>
2021-01-09 19:56:38 +0800

    first commit

---
+++ src/main/java/zhonger/cancer/bbs/controller/HelloController.java
@@ -0,0 +1,15 @@
+package zhonger.cancer.bbs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
---
+++ src/test/java/zhonger/cancer/bbs/BbsApplicationTests.java
@@ -0,0 +1,13 @@
+package zhonger.cancer.bbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
