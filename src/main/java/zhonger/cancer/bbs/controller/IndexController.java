4ab1ba711acaa76e9b7473d8298ff9e88bdecac2
komaeka <komaeka@163.com>
2021-01-11 12:04:25 +0800

    add index and bootstrap

--- src/main/java/zhonger/cancer/bbs/controller/IndexController.java
+++ src/main/java/zhonger/cancer/bbs/controller/IndexController.java
@@ -6,10 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloController {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name") String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
-    }
+public class IndexController {
+    @GetMapping("/")
+    public String index(){return "index";}
 }
