66f874414ff766fa26c07a87f8267c3c3f161ff5
komaeka <komaeka@163.com>
2021-05-12 19:09:41 +0800

    2021-5-12

--- src/main/java/zhonger/cancer/bbs/interceptor/SessionInterceptor.java
+++ src/main/java/zhonger/cancer/bbs/interceptor/SessionInterceptor.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
-import zhonger.cancer.bbs.mapper.NotificationMapper;
 import zhonger.cancer.bbs.mapper.UserMapper;
 import zhonger.cancer.bbs.model.User;
 import zhonger.cancer.bbs.model.UserExample;
--- src/main/java/zhonger/cancer/bbs/interceptor/WebConfig.java
+++ src/main/java/zhonger/cancer/bbs/interceptor/WebConfig.java
@@ -2,7 +2,6 @@ package zhonger.cancer.bbs.interceptor;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
